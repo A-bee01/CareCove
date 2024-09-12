f02cf617d47a07222ae3606651284e03767f61e9
emejulucodes <emmanueljunior433@gmail.com>
2024-09-12 02:23:54 +0100

    Refactor project structure and add TypeScript support

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+  </template>
+  <script setup lang="ts">
+  import { useStore } from '~/data_providers/donation_handler.js';
+  import { provide } from 'vue';
+  
+  const store = useStore();
+  provide('store', store);
+  
+  useHead({
+    title: 'CareCove',
+    meta: [
+      { name: 'description', content: 'Donation platform using tbDEX SDK' }
+    ]
+  })
+  </script>
+  

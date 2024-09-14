44cb847f2c175aff9430fc0705bfd517bf23f14a
emejulucodes <emmanueljunior433@gmail.com>
2024-09-14 13:33:02 +0100

    change spinner.

--- components/Spinner.vue
+++ components/Spinner.vue
@@ -1,20 +1,12 @@
 <template>
   <div class="flex justify-center items-center h-16">
-    <div class="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12"></div>
+    <img src="~/assets/loading.gif" alt="Loading..." class="w-8 h-8 animate-spin loader" />
   </div>
 </template>
 
 <style scoped>
 .loader {
-  border-top-color: #3498db;
-  animation: spin 1s ease-in-out infinite;
-}
-@keyframes spin {
-  0% {
-    transform: rotate(0deg);
-  }
-  100% {
-    transform: rotate(360deg);
-  }
+  width: 5rem;
+  height: 5rem;
 }
 </style>

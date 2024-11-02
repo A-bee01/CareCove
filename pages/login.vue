733970e2bfd02c72769bfc29ce33db751884697e
emejulucodes <emmanueljunior433@gmail.com>
2024-11-02 09:09:25 +0100

    Google signin with direct

--- pages/login.vue
+++ pages/login.vue
@@ -1,67 +1,100 @@
 <template>
-    <div class="flex flex-col min-h-screen">
-      <Header />
-      <main class="flex-1 bg-[#ffffff] dark:bg-[#ffffff] py-8 px-6 md:px-8">
-        <div class="max-w-3xl mx-auto bg-white dark:bg-[#10673b] rounded-lg shadow p-6">
-          <h2 class="text-2xl font-bold mb-4 dark:text-white">Login / Register To Donate</h2>
-  
-          <form @submit.prevent="loginWithEmail">
-            <div class="mb-4">
-              <label class="block text-gray-700 dark:text-gray-300 mb-2">Email</label>
-              <input v-model="email" type="email" required class="w-full p-2 border rounded" placeholder="Email" />
-            </div>
-            <div class="mb-4">
-              <label class="block text-gray-700 dark:text-gray-300 mb-2">Password</label>
-              <input v-model="password" type="password" required class="w-full p-2 border rounded" placeholder="Password" />
-            </div>
-            <button type="submit" class="inline-flex items-center justify-center rounded-md text-sm font-medium bg-blue-500 text-white hover:bg-blue-600 h-10 px-4 py-2">
-             Continue
-            </button>
-          </form>
-  
-          <div class="mt-4">
-            <button @click="loginWithGoogle" class="inline-flex items-center justify-center rounded-md text-sm font-medium bg-red-500 text-white hover:bg-red-600 h-10 px-4 py-2">
-             Continue with Google
-            </button>
+  <div class="flex flex-col min-h-screen">
+    <Header />
+    <main class="flex-1 bg-[#ffffff] dark:bg-[#ffffff] py-8 px-6 md:px-8">
+      <div
+        class="max-w-3xl mx-auto bg-white dark:bg-[#10673b] rounded-lg shadow p-6"
+      >
+        <h2 class="text-2xl font-bold mb-4 dark:text-white">
+          Login / Register To Donate
+        </h2>
+
+        <form @submit.prevent="loginWithEmail">
+          <div class="mb-4">
+            <label class="block text-gray-700 dark:text-gray-300 mb-2"
+              >Email</label
+            >
+            <input
+              v-model="email"
+              type="email"
+              required
+              class="w-full p-2 border rounded"
+              placeholder="Email"
+            />
           </div>
+          <div class="mb-4">
+            <label class="block text-gray-700 dark:text-gray-300 mb-2"
+              >Password</label
+            >
+            <input
+              v-model="password"
+              type="password"
+              required
+              class="w-full p-2 border rounded"
+              placeholder="Password"
+            />
+          </div>
+          <button
+            type="submit"
+            class="inline-flex items-center justify-center rounded-md text-sm font-medium bg-blue-500 text-white hover:bg-blue-600 h-10 px-4 py-2"
+          >
+            Continue
+          </button>
+        </form>
+
+        <div class="mt-4">
+          <button
+            @click="loginWithGoogle"
+            class="inline-flex items-center justify-center rounded-md text-sm font-medium bg-red-500 text-white hover:bg-red-600 h-10 px-4 py-2"
+          >
+            Continue with Google
+          </button>
         </div>
-      </main>
-    </div>
-  </template>
-  
-  <script setup>
-  import { ref } from 'vue';
-  import { useRouter } from 'vue-router';
-  import { signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, getAuth } from 'firebase/auth';
-  import Header from '~/components/Header.vue';
-  
-  const router = useRouter();
-  const auth = getAuth();
-  const email = ref('');
-  const password = ref('');
-  
-  const loginWithEmail = async () => {
-    try {
+      </div>
+    </main>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signInWithPopup,
+  GoogleAuthProvider,
+  getAuth,
+  signInWithRedirect,
+} from "firebase/auth";
+import Header from "~/components/Header.vue";
+
+const router = useRouter();
+const auth = getAuth();
+const email = ref("");
+const password = ref("");
+
+const loginWithEmail = async () => {
+  try {
+    await createUserWithEmailAndPassword(auth, email.value, password.value);
+    router.push("/dashboard"); // Redirect after successful login
+  } catch (error) {
+    console.error("Error logging in with email/password:", error);
+    //if user exists, log them in
+    if (error.code === "auth/email-already-in-use") {
+      console.error("User not found. Please sign up.");
       await signInWithEmailAndPassword(auth, email.value, password.value);
-      router.push('/dashboard'); // Redirect after successful login
-    } catch (error) {
-      console.error('Error logging in with email/password:', error);
-      if (error.code === 'auth/user-not-found') {
-        console.error('User not found. Please sign up.');
-        await createUserWithEmailAndPassword(auth, email.value, password.value);
-        router.push('/dashboard'); // Redirect after successful signup
-      }
+      router.push("/dashboard"); // Redirect after successful signup
     }
-  };
-  
-  const loginWithGoogle = async () => {
-    const provider = new GoogleAuthProvider();
-    try {
-      await signInWithPopup(auth, provider);
-      router.push('/dashboard'); // Redirect after successful login
-    } catch (error) {
-      console.error('Error logging in with Google:', error);
-    }
-  };
-  </script>
-  
+  }
+};
+
+const loginWithGoogle = async () => {
+  const provider = new GoogleAuthProvider();
+  try {
+    await signInWithRedirect(auth, provider);
+    router.push("/dashboard"); // Redirect after successful login
+  } catch (error) {
+    console.error("Error logging in with Google:", error);
+  }
+};
+</script>
